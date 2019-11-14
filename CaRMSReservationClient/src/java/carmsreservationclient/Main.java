544ee39341589c8528d65923d0afa53b70c1f93e
Zhi Xuan <zhixuan.lee@u.nus.edu>
2019-11-14 19:09:45 +0800

    feat: added calculation of rental fees

--- CaRMSReservationClient/src/java/carmsreservationclient/Main.java
+++ CaRMSReservationClient/src/java/carmsreservationclient/Main.java
@@ -1,12 +1,20 @@
 package carmsreservationclient;
 
+import ejb.session.stateless.CategoryEntitySessionBeanRemote;
 import ejb.session.stateless.CustomerEntitySessionBeanRemote;
+import ejb.session.stateless.ModelEntitySessionBeanRemote;
 import ejb.session.stateless.OutletEntitySessionBeanRemote;
 import ejb.session.stateless.RentalReservationEntitySessionBeanRemote;
 import ejb.session.stateless.ReservationSessionBeanRemote;
 import javax.ejb.EJB;
 
 public class Main {
+
+    @EJB(name = "ModelEntitySessionBeanRemote")
+    private static ModelEntitySessionBeanRemote modelEntitySessionBean;
+
+    @EJB(name = "CategoryEntitySessionBeanRemote")
+    private static CategoryEntitySessionBeanRemote categoryEntitySessionBean;
 
     @EJB(name = "RentalReservationEntitySessionBeanRemote")
     private static RentalReservationEntitySessionBeanRemote rentalReservationEntitySessionBeanRemote;
@@ -23,7 +31,7 @@ public class Main {
     
     
     public static void main(String[] args) {
-        MainApp mainApp = new MainApp(customerEntitySessionBeanRemote, reservationSessionBeanRemote, outletEntitySessionBeanRemote, rentalReservationEntitySessionBeanRemote);
+        MainApp mainApp = new MainApp(categoryEntitySessionBean, customerEntitySessionBeanRemote, modelEntitySessionBean, reservationSessionBeanRemote, outletEntitySessionBeanRemote, rentalReservationEntitySessionBeanRemote);
         mainApp.runApp();
         
     }
